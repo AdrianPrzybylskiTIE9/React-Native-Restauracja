8283fb02486b7bd55dae98d647ffcb1b5c3567cd
Yndh <adrian.przybylski@cyfroweszkoly.pl>
2023-03-26 15:18:15 +0200

    Remove Py File

--- assets/main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = 'F:/Szkola/PAM/React-Native-Restauracja/assets/icons'
-
-print('icons = {')
-for el in os.listdir((path)):
-    print(f"'{el[:-4].replace('-',' ')}': require('../assets/icons/{el}'),")
-print('}')
